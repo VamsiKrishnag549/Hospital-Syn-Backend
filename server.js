c7c4f9e96d084140aff1aaabd86d2572d30a4ada
VamsiKrishnag549 <vamsikrishnagokaraju123@gmail.com>
2026-03-28 16:01:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./models/User");
+const User = require("./User");
 const app = express();
 
 app.use(cors());
