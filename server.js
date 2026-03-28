022b677582fb2eab1e15566b951de9ec8a9a972c
VamsiKrishnag549 <vamsikrishnagokaraju123@gmail.com>
2026-03-28 16:38:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("🔥 Server running on https://hospital-syn-backend.onrender.com");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🔥 Server running on port ${PORT}`);
 });
 
 app.post("/register", async (req, res) => {
