56347aef7e2cfb93ac23d82734fde6be33c0b8da
VamsiKrishnag549 <vamsikrishnagokaraju123@gmail.com>
2026-03-28 17:46:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./user");
+const User = require("./User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const app = express();
